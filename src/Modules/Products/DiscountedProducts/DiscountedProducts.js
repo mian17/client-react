bf51f2729b52995c3e616ec02b9145fe69656140
night <nightknight011@gmail.com>
2022-06-03 13:33:33 +0700

    MUI alternative to Sort by was added in FilterProductItems.js. Modularized ProductItem.js for future usage.

--- src/Modules/Products/DiscountedProducts/DiscountedProducts.js
+++ src/Modules/Products/DiscountedProducts/DiscountedProducts.js
@@ -11,23 +11,65 @@ import pd_4 from "../../../Assets/img/product/discount/pd-4.jpg";
 import pd_5 from "../../../Assets/img/product/discount/pd-5.jpg";
 import pd_6 from "../../../Assets/img/product/discount/pd-6.jpg";
 
+const images = [
+  {
+    imageUrl: pd_1,
+    productName: "Hạt khô",
+    discountedPrice: 36000,
+    oldPrice: 30000,
+    discountedRatio: 20,
+    category: "Trái cây khô",
+  },
+  {
+    imageUrl: pd_2,
+    productName: "Bánh sandwich",
+    discountedPrice: 23000,
+    oldPrice: 25000,
+    discountedRatio: 10,
+    category: "Thức ăn nhanh",
+  },
+  {
+    imageUrl: pd_3,
+    productName: "Nước trái cây",
+    discountedPrice: 10000,
+    oldPrice: 15000,
+    discountedRatio: 12,
+    category: "Nước uống",
+  },
+  {
+    imageUrl: pd_4,
+    productName: "Xoài",
+    discountedPrice: 30000,
+    oldPrice: 50000,
+    discountedRatio: 25,
+    category: "Trái cây",
+  },
+  {
+    imageUrl: pd_5,
+    productName: "Bánh Hamburger",
+    discountedPrice: 300000,
+    oldPrice: 100000,
+    discountedRatio: 26,
+    category: "Thức ăn nhanh",
+  },
+  {
+    imageUrl: pd_6,
+    productName: "Nho Mỹ",
+    discountedPrice: 400000,
+    oldPrice: 200000,
+    discountedRatio: 50,
+    category: "Trái cây",
+  },
+];
+
 const categoriesBreakpointsOption = {
   320: {
-    slidesPerView: 1,
-  },
-  375: {
     slidesPerView: 1,
   },
   479: {
     slidesPerView: 2,
   },
-  576: {
-    slidesPerView: 2,
-  },
-  640: {
-    slidesPerView: 3,
-  },
-  768: {
+  992: {
     slidesPerView: 3,
   },
 };
@@ -47,199 +89,210 @@ class DiscountedProducts extends Component {
           slidesPerView={3}
           className="row"
         >
-          <SwiperSlide className="col-lg-4">
-            <ProductItem />
-          </SwiperSlide>
-          <SwiperSlide className="col-lg-4">
-            <div className="product__discount__item">
-              <div
-                className="product__discount__item__pic"
-                style={{
-                  background: `url("${pd_3}") no-repeat center`,
-                }}
-              >
-                <div className="product__discount__percent">-20%</div>
-                <ul className="product__item__pic__hover">
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-heart"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-retweet"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-shopping-cart"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="product__discount__item__text">
-                <span>Vegetables</span>
-                <h5>
-                  <a href="/">Vegetables’package</a>
-                </h5>
-                <div className="product__item__price">
-                  $30.00 <span>$36.00</span>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="col-lg-4">
-            <div className="product__discount__item">
-              <div
-                className="product__discount__item__pic"
-                style={{
-                  background: `url("${pd_4}") no-repeat center`,
-                }}
-              >
-                <div className="product__discount__percent">-20%</div>
-                <ul className="product__item__pic__hover">
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-heart"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-retweet"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-shopping-cart"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="product__discount__item__text">
-                <span>Vegetables</span>
-                <h5>
-                  <a href="/">Vegetables’package</a>
-                </h5>
-                <div className="product__item__price">
-                  $30.00 <span>$36.00</span>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="col-lg-4">
-            <div className="product__discount__item">
-              <div
-                className="product__discount__item__pic"
-                style={{
-                  background: `url("${pd_2}") no-repeat center`,
-                }}
-              >
-                <div className="product__discount__percent">-20%</div>
-                <ul className="product__item__pic__hover">
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-heart"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-retweet"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-shopping-cart"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="product__discount__item__text">
-                <span>Vegetables</span>
-                <h5>
-                  <a href="/">Vegetables’package</a>
-                </h5>
-                <div className="product__item__price">
-                  $30.00 <span>$36.00</span>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="col-lg-4">
-            <div className="product__discount__item">
-              <div
-                className="product__discount__item__pic"
-                style={{
-                  background: `url("${pd_5}") no-repeat center`,
-                }}
-              >
-                <div className="product__discount__percent">-20%</div>
-                <ul className="product__item__pic__hover">
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-heart"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-retweet"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-shopping-cart"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="product__discount__item__text">
-                <span>Vegetables</span>
-                <h5>
-                  <a href="/">Vegetables’package</a>
-                </h5>
-                <div className="product__item__price">
-                  $30.00 <span>$36.00</span>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="col-lg-4">
-            <div className="product__discount__item">
-              <div
-                className="product__discount__item__pic"
-                style={{
-                  background: `url("${pd_6}") no-repeat center`,
-                }}
-              >
-                <div className="product__discount__percent">-20%</div>
-                <ul className="product__item__pic__hover">
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-heart"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-retweet"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/">
-                      <i className="fa fa-shopping-cart"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="product__discount__item__text">
-                <span>Vegetables</span>
-                <h5>
-                  <a href="/">Vegetables’package</a>
-                </h5>
-                <div className="product__item__price">
-                  $30.00 <span>$36.00</span>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
+          {images.map((image) => {
+            return (
+              <SwiperSlide className="col-lg-4">
+                <ProductItem
+                  imageUrl={image.imageUrl}
+                  productName={image.productName}
+                  discountedPrice={image.discountedPrice}
+                  oldPrice={image.oldPrice}
+                  discountedRatio={image.discountedRatio}
+                  category={image.category}
+                />
+              </SwiperSlide>
+            );
+          })}
+          {/*<SwiperSlide className="col-lg-4">*/}
+          {/*  <div className="product__discount__item">*/}
+          {/*    <div*/}
+          {/*      className="product__discount__item__pic"*/}
+          {/*      style={{*/}
+          {/*        background: `url("${pd_3}") no-repeat center`,*/}
+          {/*      }}*/}
+          {/*    >*/}
+          {/*      <div className="product__discount__percent">-20%</div>*/}
+          {/*      <ul className="product__item__pic__hover">*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-heart"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-retweet"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-shopping-cart"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*      </ul>*/}
+          {/*    </div>*/}
+          {/*    <div className="product__discount__item__text">*/}
+          {/*      <span>Vegetables</span>*/}
+          {/*      <h5>*/}
+          {/*        <a href="/">Vegetables’package</a>*/}
+          {/*      </h5>*/}
+          {/*      <div className="product__item__price">*/}
+          {/*        $30.00 <span>$36.00</span>*/}
+          {/*      </div>*/}
+          {/*    </div>*/}
+          {/*  </div>*/}
+          {/*</SwiperSlide>*/}
+          {/*<SwiperSlide className="col-lg-4">*/}
+          {/*  <div className="product__discount__item">*/}
+          {/*    <div*/}
+          {/*      className="product__discount__item__pic"*/}
+          {/*      style={{*/}
+          {/*        background: `url("${pd_4}") no-repeat center`,*/}
+          {/*      }}*/}
+          {/*    >*/}
+          {/*      <div className="product__discount__percent">-20%</div>*/}
+          {/*      <ul className="product__item__pic__hover">*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-heart"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-retweet"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-shopping-cart"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*      </ul>*/}
+          {/*    </div>*/}
+          {/*    <div className="product__discount__item__text">*/}
+          {/*      <span>Vegetables</span>*/}
+          {/*      <h5>*/}
+          {/*        <a href="/">Vegetables’package</a>*/}
+          {/*      </h5>*/}
+          {/*      <div className="product__item__price">*/}
+          {/*        $30.00 <span>$36.00</span>*/}
+          {/*      </div>*/}
+          {/*    </div>*/}
+          {/*  </div>*/}
+          {/*</SwiperSlide>*/}
+          {/*<SwiperSlide className="col-lg-4">*/}
+          {/*  <div className="product__discount__item">*/}
+          {/*    <div*/}
+          {/*      className="product__discount__item__pic"*/}
+          {/*      style={{*/}
+          {/*        background: `url("${pd_2}") no-repeat center`,*/}
+          {/*      }}*/}
+          {/*    >*/}
+          {/*      <div className="product__discount__percent">-20%</div>*/}
+          {/*      <ul className="product__item__pic__hover">*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-heart"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-retweet"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-shopping-cart"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*      </ul>*/}
+          {/*    </div>*/}
+          {/*    <div className="product__discount__item__text">*/}
+          {/*      <span>Vegetables</span>*/}
+          {/*      <h5>*/}
+          {/*        <a href="/">Vegetables’package</a>*/}
+          {/*      </h5>*/}
+          {/*      <div className="product__item__price">*/}
+          {/*        $30.00 <span>$36.00</span>*/}
+          {/*      </div>*/}
+          {/*    </div>*/}
+          {/*  </div>*/}
+          {/*</SwiperSlide>*/}
+          {/*<SwiperSlide className="col-lg-4">*/}
+          {/*  <div className="product__discount__item">*/}
+          {/*    <div*/}
+          {/*      className="product__discount__item__pic"*/}
+          {/*      style={{*/}
+          {/*        background: `url("${pd_5}") no-repeat center`,*/}
+          {/*      }}*/}
+          {/*    >*/}
+          {/*      <div className="product__discount__percent">-20%</div>*/}
+          {/*      <ul className="product__item__pic__hover">*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-heart"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-retweet"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-shopping-cart"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*      </ul>*/}
+          {/*    </div>*/}
+          {/*    <div className="product__discount__item__text">*/}
+          {/*      <span>Vegetables</span>*/}
+          {/*      <h5>*/}
+          {/*        <a href="/">Vegetables’package</a>*/}
+          {/*      </h5>*/}
+          {/*      <div className="product__item__price">*/}
+          {/*        $30.00 <span>$36.00</span>*/}
+          {/*      </div>*/}
+          {/*    </div>*/}
+          {/*  </div>*/}
+          {/*</SwiperSlide>*/}
+          {/*<SwiperSlide className="col-lg-4">*/}
+          {/*  <div className="product__discount__item">*/}
+          {/*    <div*/}
+          {/*      className="product__discount__item__pic"*/}
+          {/*      style={{*/}
+          {/*        background: `url("${pd_6}") no-repeat center`,*/}
+          {/*      }}*/}
+          {/*    >*/}
+          {/*      <div className="product__discount__percent">-20%</div>*/}
+          {/*      <ul className="product__item__pic__hover">*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-heart"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-retweet"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*        <li>*/}
+          {/*          <a href="/">*/}
+          {/*            <i className="fa fa-shopping-cart"></i>*/}
+          {/*          </a>*/}
+          {/*        </li>*/}
+          {/*      </ul>*/}
+          {/*    </div>*/}
+          {/*    <div className="product__discount__item__text">*/}
+          {/*      <span>Vegetables</span>*/}
+          {/*      <h5>*/}
+          {/*        <a href="/">Vegetables’package</a>*/}
+          {/*      </h5>*/}
+          {/*      <div className="product__item__price">*/}
+          {/*        $30.00 <span>$36.00</span>*/}
+          {/*      </div>*/}
+          {/*    </div>*/}
+          {/*  </div>*/}
+          {/*</SwiperSlide>*/}
         </Swiper>
       </div>
     );
--- src/Modules/Products/FilterProductItems/FilterProductItems.js
+++ src/Modules/Products/FilterProductItems/FilterProductItems.js
@@ -1,36 +1,56 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-class FilterProductItems extends Component {
-  render() {
-    return (
-      <div className="filter__item">
-        <div className="row">
-          <div className="col-lg-4 col-md-5">
-            <div className="filter__sort">
-              <span>Sort By</span>
-              <select>
-                <option value="0">Default</option>
-                <option value="0">Default</option>
-              </select>
-            </div>
+function FilterProductItems() {
+  const [filter, setFilter] = useState("");
+
+  const handleChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  return (
+    <div className="filter__item">
+      <div className="row align-items-center">
+        <div className="col-lg-4 col-md-5">
+          <div className="filter__sort">
+            <FormControl fullWidth>
+              <InputLabel id="product-filter">Sắp xếp theo</InputLabel>
+              <Select
+                labelId="product-filter"
+                id="product-filter"
+                value={filter}
+                label="Sắp xếp theo"
+                onChange={handleChange}
+              >
+                <MenuItem value={"price-goes-up"}>
+                  Giá: từ thấp đến cao
+                </MenuItem>
+                <MenuItem value={"price-goes-down"}>
+                  Giá: từ cao đến thấp
+                </MenuItem>
+              </Select>
+            </FormControl>
           </div>
-          <div className="col-lg-4 col-md-4">
-            <div className="filter__found">
-              <h6>
-                <span>16</span> Products found
-              </h6>
-            </div>
+        </div>
+        <div className="col-lg-5 col-md-4">
+          <div className="filter__found">
+            <h6>
+              <span>16</span> sản phẩm được tìm thấy
+            </h6>
           </div>
-          <div className="col-lg-4 col-md-3">
-            <div className="filter__option">
-              <span className="icon_grid-2x2"></span>
-              <span className="icon_ul"></span>
-            </div>
+        </div>
+        <div className="col-lg-3 col-md-3">
+          <div className="filter__option">
+            <span className="icon_grid-2x2"></span>
+            <span className="icon_ul"></span>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default FilterProductItems;
